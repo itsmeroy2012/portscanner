8d3e41d2afe19637f3ce10777cd346ab4c68c30c
kira0204 <rshtmudgal@gmail.com>
2017-03-17 23:13:22 +0530

    Code commented

--- portscanner_ir.py
+++ portscanner_ir.py
@@ -5,6 +5,7 @@
 #usage:- ports- Enter the port number you want to scan separated by a comma for example:- -p 20,21,22 (scans port 20,21,22 for the above ip addresses)
 #usage:- 'python portscanner_ir.py -h 172.29.43.* -p 20,21,23'
 #Run in a linux or ubuntu terminal
+
 import optparse
 import socket
 def connect(ip,port):
@@ -21,6 +22,8 @@ def connect(ip,port):
 
 
 def main():
+
+	#Used to parse argumentss
 	parser=optparse.OptionParser('usage%prog '+' -H <ip address range e.g. 192.168.65.*> -p <target ports separated by comma> ')
 	parser.add_option('-H', dest="t1host" , type="string" , help="specify target host range")
 	parser.add_option('-p' , dest="t1port" , type="string" , help="specify target port separated by ',' ")
@@ -30,9 +33,12 @@ def main():
 	tports = str(options.t1port)
 	tports=tports.split(',')
 	ip1=str(t1host).strip('*')
+
+	#If host or port is null
 	if (t1host == None) | (t1port == None):
 		print parser.usage
 		exit(0)
+	#Scanning ports
 	for x in range(1,255):
 		ip= ip1+str(x)
 		for port in tports:
@@ -43,6 +49,6 @@ def main():
 				portlog.append(port)
 				print "[+] "+ip+" : "+banner
 	
-
+#Initiating main
 if __name__ == "__main__":
 	main()
